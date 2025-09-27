793b2092f86c699f7251401f3ad1a81ae2564e2f
olalekan <olalekanotuyiga@gmail.com>
2025-09-27 17:13:01 +0100

    text, view and button summary

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,50 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+
+  const clickbutton = () => {
+    console.log("button clicked")
+  }
+
+  /* for touchableopacity */
+  const onPress = () => {
+    console.log("opacity button pressed")
+  }
+
+
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "hsl(200, 100%, 50%)",
       }}
     >
-      <Text>hello brookly</Text>
-      <Text>hello world</Text>
+      <Text style={styles.text}>one ticket</Text>
+      <Text >global ticket</Text>
+      <Button title="Click me!" onPress={() => alert("Hello!")} />
+      <Button title="press me!" onPress={clickbutton} />
+      <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={onPress}>
+          <Text>Press Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textTransform: "uppercase",
+    color: "white",
+  },
+
+  button: {
+    marginTop: 20,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
